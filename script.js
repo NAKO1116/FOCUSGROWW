4db23027e28832d7ac873c0fc2fab5f9b48b8fd0
NAKO1116 <k24as602@st.kyusan-u.ac.jp>
2026-07-02 12:12:42 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,552 @@
+// ===== タイマー =====
+
+let time = 300;
+let timer;
+let running = false;
+
+// ===== 成長 =====
+
+let level = Number(localStorage.getItem("level")) || 1;
+let exp = Number(localStorage.getItem("exp")) || 0;
+const expTable = {
+    1:3,
+    2:7,
+    3:10,
+    4:10,
+    5:10,
+    6:15,
+    7:15,
+    8:15,
+    9:15,
+    10:15,
+    11:20,
+    12:20,
+    13:20,
+    14:20,
+    15:20,
+    16:25,
+    17:25,
+    18:25,
+    19:25
+};
+
+let maxExp =
+expTable[level] || 25;
+
+// ===== 継続日数 =====
+
+let streakDays =
+Number(localStorage.getItem("streakDays")) || 1;
+
+let bestRecord =
+Number(localStorage.getItem("bestRecord")) || 1;
+
+// ===== 集中回数 =====
+
+let focusCount =
+Number(
+    localStorage.getItem(
+        "focusCount"
+    )
+) || 0;
+
+// ===== 要素 =====
+
+const timerDisplay =
+document.getElementById("timer");
+
+const levelText =
+document.getElementById("levelText");
+
+const nextExp =
+document.getElementById("nextExp");
+
+const expBar =
+document.getElementById("expBar");
+
+const shareExpBar =
+document.getElementById("shareExpBar");
+
+const character =
+document.getElementById("character");
+
+const alarmSound =
+document.getElementById("alarmSound");
+
+// ===== BGM =====
+
+const bgm =
+document.getElementById("bgm");
+
+const bgmBtn =
+document.getElementById("bgmBtn");
+
+let bgmPlaying = false;
+
+// ===== 保存 =====
+
+function saveData(){
+
+    localStorage.setItem("level",level);
+    localStorage.setItem("exp",exp);
+    localStorage.setItem("maxExp",maxExp);
+
+    localStorage.setItem(
+        "streakDays",
+        streakDays
+    );
+
+    localStorage.setItem(
+        "bestRecord",
+        bestRecord
+    );
+
+    localStorage.setItem(
+        "focusCount",
+        focusCount
+    );
+}
+
+// ===== キャラ進化 =====
+
+function updateCharacter(){
+
+    if(focusCount >= 20){
+
+        character.src =
+        "character4.png";
+
+    }else if(focusCount >= 10){
+
+        character.src =
+        "character3.png";
+
+    }else if(focusCount >= 3){
+
+        character.src =
+        "character2.png";
+
+    }else{
+
+        character.src =
+        "character1.png";
+
+    }
+
+}
+
+// ===== レベル更新 =====
+
+function updateLevelUI(){
+
+    let formName = "";
+
+    // 形態名
+    if(level >= 16){
+
+        formName = "ユグドラ";
+
+    }else if(level >= 11){
+
+        formName = "シルヴァ";
+
+    }else if(level >= 6){
+
+        formName = "フィーネ";
+
+    }else{
+
+        formName = "ミーネ";
+    }
+
+    // 必要経験値
+    maxExp = expTable[level] || 25;
+
+    // 左側
+    levelText.textContent =
+    `${formName} Lv.${level}`;
+
+    // 右側
+    document.getElementById(
+        "shareLevel"
+    ).textContent =
+    `Lv.${level}`;
+    document.getElementById(
+    "shareForm"
+    ).textContent =
+    formName;
+
+    // 経験値バー
+    const percent =
+    (exp / maxExp) * 100;
+
+    if(expBar){
+
+        expBar.style.width =
+        percent + "%";
+    }
+
+    const shareBar =
+    document.getElementById(
+        "shareExpBar"
+    );
+
+    if(shareBar){
+
+        shareBar.style.width =
+        percent + "%";
+    }
+
+    // 次レベル表示
+    let remain = 0;
+
+     if(level < 6){
+
+    remain = 6 - level;
+
+     }else if(level < 11){
+
+    remain = 11 - level;
+
+     }else if(level < 16){
+
+    remain = 16 - level;
+
+     }else{
+
+    remain = 20 - level;
+
+     }
+
+nextExp.textContent =
+`次の進化まで あと${remain}回`;
+    updateCharacter();
+
+    saveData();
+}
+// ===== 経験値 =====
+
+function gainExp(amount){
+
+    exp += amount;
+
+    while(exp >= maxExp){
+
+        exp -= maxExp;
+
+        level++;
+
+        maxExp += 20;
+
+        alert(
+            `レベルアップ！ Lv.${level}`
+        );
+    }
+
+    updateLevelUI();
+}
+
+// ===== タイマー =====
+
+function updateTimer(){
+
+    const m =
+    Math.floor(time/60);
+
+    const s =
+    time%60;
+
+    timerDisplay.textContent =
+    `${String(m).padStart(2,"0")}:${String(s).padStart(2,"0")}`;
+}
+
+document
+.getElementById("startBtn")
+.addEventListener("click",()=>{
+
+    if(running) return;
+
+    running = true;
+
+    timer = setInterval(()=>{
+
+        time--;
+
+        updateTimer();
+
+        if(time <= 0){
+
+            clearInterval(timer);
+
+            running = false;
+
+            gainExp(10);
+
+            // 集中回数追加
+            focusCount++;
+
+            // キャラ更新
+            updateCharacter();
+
+            // 喜びモーション
+             happyMotion();
+
+            streakDays++;
+
+            if(streakDays > bestRecord){
+
+                bestRecord =
+                streakDays;
+            }
+
+            document
+            .getElementById(
+                "streakDays"
+            ).textContent =
+            `${streakDays}日`;
+
+            document
+            .getElementById(
+                "bestRecord"
+            ).textContent =
+            `ベスト記録：${bestRecord}日`;
+
+            saveData();
+            
+            alarmSound.currentTime = 0;
+            
+            alarmSound.play();
+
+            alert(
+            "5分集中達成！ EXP+10"
+            );
+
+            time = 300;
+
+            updateTimer();
+        }
+
+    },1000);
+
+});
+
+//==== リセット　====//
+
+document
+.getElementById("resetBtn")
+.addEventListener("click",()=>{
+
+    clearInterval(timer);
+
+    running = false;
+
+    time = 300;
+
+    updateTimer();
+
+});
+
+
+
+// ===== シェア =====
+
+document
+.getElementById("shareBtn")
+.addEventListener("click",()=>{
+
+    const text =
+    `FocusGrowでLv.${level}まで成長しました！`;
+
+    navigator.clipboard.writeText(text);
+
+    alert(
+    "シェア用テキストをコピーしました！"
+    );
+
+});
+
+// ===== 初期表示 =====
+
+updateTimer();
+updateLevelUI();
+updateCharacter();
+
+document.getElementById(
+    "streakDays"
+).textContent =
+`${streakDays}日`;
+
+document.getElementById(
+    "bestRecord"
+).textContent =
+`ベスト記録：${bestRecord}日`;
+
+// ===== ToDo =====
+
+let todos =
+JSON.parse(
+    localStorage.getItem("todos")
+) || [];
+
+const todoList =
+document.getElementById("todoList");
+
+const todoInput =
+document.getElementById("todoInput");
+
+const addTodoBtn =
+document.getElementById("addTodoBtn");
+
+// 表示
+
+function renderTodos(){
+
+    todoList.innerHTML = "";
+
+    todos.forEach((todo,index)=>{
+
+        const li =
+        document.createElement("li");
+
+        li.classList.add("todo-item");
+
+        li.innerHTML = `
+            <span>${todo}</span>
+            <button
+            class="delete-btn"
+            onclick="deleteTodo(${index})">
+                削除
+            </button>
+        `;
+
+        todoList.appendChild(li);
+
+    });
+
+    localStorage.setItem(
+        "todos",
+        JSON.stringify(todos)
+    );
+}
+
+// 追加
+
+addTodoBtn.addEventListener("click",()=>{
+
+    const text =
+    todoInput.value.trim();
+
+    if(text === "") return;
+
+    todos.push(text);
+
+    todoInput.value = "";
+
+    renderTodos();
+
+});
+
+// Enterでも追加
+
+todoInput.addEventListener("keypress",(e)=>{
+
+    if(e.key === "Enter"){
+
+        addTodoBtn.click();
+    }
+
+});
+
+// 削除
+
+function deleteTodo(index){
+
+    todos.splice(index,1);
+
+    renderTodos();
+}
+
+// 初回表示
+
+renderTodos();
+
+
+
+// ===== まばたき =====
+
+setInterval(()=>{
+
+  const current =
+  character.src;
+
+  character.style.opacity =
+  "0.7";
+
+  setTimeout(()=>{
+
+    character.style.opacity =
+    "1";
+
+  },150);
+
+},4000);
+
+
+// ===== 喜びモーション =====
+
+function happyMotion(){
+
+  character.classList
+  .add("happy");
+
+  setTimeout(()=>{
+
+    character.classList
+    .remove("happy");
+
+  },800);
+
+}
+
+// ===== BGM ON/OFF =====
+
+bgm.volume = 0.3;
+
+bgmBtn.addEventListener("click", async () => {
+
+    if (bgmPlaying) {
+
+        bgm.pause();
+
+        bgm.currentTime = 0;
+
+        bgmPlaying = false;
+
+        bgmBtn.textContent = "🎵 BGM";
+
+    } else {
+
+        try {
+
+            await bgm.play();
+
+            bgmPlaying = true;
+
+            bgmBtn.textContent = "🔇 BGM";
+
+        } catch (error) {
+
+            console.error(error);
+
+            alert("BGMを再生できませんでした。");
+
+        }
+
+    }
+
+});
+
+// =========================
+// 画面サイズに合わせて縮小・拡大
+// =========================
+
